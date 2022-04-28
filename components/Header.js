4991ff02761f07aed9935e782f106d85dba25fe3
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-04-27 17:54:41 -0700

    04. Style the header section

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,8 @@ import {
 
 export default function Header() {
   return (
-    <div>
-      <div className="">
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+      <div className="flex">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
         <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
@@ -20,6 +20,7 @@ export default function Header() {
         src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
         width={100}
         height={100}
+        className="cursor-pointer active:brightness-110"
         alt=""
       />
     </div>
